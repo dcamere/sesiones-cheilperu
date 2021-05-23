ba9cc06c63a4df6c8e5f537fcffd195ae5e34ae0
Diego Camere <dcamere@gmail.com>
2021-05-23 12:36:32 -0500

    personalized error mesages

--- ejemplos/sesion4/app.js
+++ ejemplos/sesion4/app.js
@@ -1,20 +1,36 @@
 const Axios = require('axios');
 const Uuid = require('node-uuid');
 exports.handler = async (event) => {
-    const tid = Uuid();
-    const r = await Axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    try {
+        const tid = Uuid();
+        const r = await Axios.get('https://jsonplaceholder.typicode.com/todos/1');
+        
+        LOG(tid, "INFO", "connected to db", r.data)
+        // TODO implement
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify('Hello from Lambda!'),
+        };
+        response = {
+            statusCode: 200,
+            body: JSON.stringify('Hello from lambda!')
+        }
+    } catch (e) {
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({message: e})
+        }
+    } finally {
+        return response;
+    }
+};
+
+function LOG (tid, level = "INFO", message, data = {}) {
     console.log(JSON.stringify({
         tid,
         createdAt: new Date(),
-        level: "INFO",
-        message: "message",
-        data: r.data
+        level,
+        message,
+        data: data
     }))
-    console.log(r.data);
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
+}
