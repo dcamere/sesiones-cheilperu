8421e728c3c720dde751e287d6c18d2a8ee17f1e
Diego Camere <dcamere@gmail.com>
2021-05-23 12:40:01 -0500

    fixes

--- ejemplos/sesion4/app.js
+++ ejemplos/sesion4/app.js
@@ -2,26 +2,24 @@ const Axios = require('axios');
 const Uuid = require('node-uuid');
 exports.handler = async (event) => {
     let response;
-    
+
     try {
         const tid = Uuid();
         const r = await Axios.get('https://jsonplaceholder.typicode.com/todos/1');
         
-        LOG(tid, "INFO", "connected to db", r.data)
+        
         // TODO implement
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify('Hello from Lambda!'),
-        };
         response = {
             statusCode: 200,
             body: JSON.stringify('Hello from lambda!')
         }
+        LOG(tid, "INFO", "try", response)
     } catch (e) {
         response = {
             statusCode: 500,
             body: JSON.stringify({message: e})
         }
+        LOG(tid, "INFO", "catch", response)
     } finally {
         return response;
     }
