8a2d0262778c983ffc0c25ffd2938a362a02726e
Diego Camere <dcamere@gmail.com>
2021-05-23 11:22:15 -0500

    added lambda

---
+++ ejemplos/sesion4/app.js
@@ -0,0 +1,11 @@
+const Axios = require('axios');
+exports.handler = async (event) => {
+    const r = await Axios.get('https://google.com');
+    console.log(r);
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
