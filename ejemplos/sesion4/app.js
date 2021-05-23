6314588b67ff44de96bb41b1af60e81134fda54a
Diego Camere <dcamere@gmail.com>
2021-05-23 12:28:10 -0500

    added log format

--- ejemplos/sesion4/app.js
+++ ejemplos/sesion4/app.js
@@ -1,6 +1,15 @@
 const Axios = require('axios');
+const Uuid = require('node-uuid');
 exports.handler = async (event) => {
+    const tid = Uuid();
     const r = await Axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(JSON.stringify({
+        tid,
+        createdAt: new Date(),
+        level: "INFO",
+        message: "message",
+        data: r.data
+    }))
     console.log(r.data);
     // TODO implement
     const response = {
