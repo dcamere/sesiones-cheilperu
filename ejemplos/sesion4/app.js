e391d369bf5c4c1388678fcedda356dc766eaa63
Diego Camere <dcamere@gmail.com>
2021-05-23 12:17:52 -0500

    changed axios log

--- ejemplos/sesion4/app.js
+++ ejemplos/sesion4/app.js
@@ -1,6 +1,6 @@
 const Axios = require('axios');
 exports.handler = async (event) => {
-    const r = await Axios.get('https://google.com');
+    const r = await Axios.get('https://jsonplaceholder.typicode.com/todos/1');
     console.log(r);
     // TODO implement
     const response = {
